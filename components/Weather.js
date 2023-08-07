d65a3fe43d1270c964961be160e8a6532fb1181d
NATTHAPHONG.P <6110110038@psu.ac.th>
2023-08-07 11:41:35 +0700

    Part-3

--- components/Weather.js
+++ components/Weather.js
@@ -1,28 +1,54 @@
 import React, { useState } from "react";
 import Forecast from "./Forecast";
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View, Text } from "react-native";
 
 export default function Weather(props) {
   const [forecastInfo, setForecastInfo] = useState({
-    main: "-",
-    description: "-",
+    main: "main",
+    description: "description",
     temp: 0,
   });
 
   return (
-    <ImageBackground
-      source={require("../bg.png")}
-      style={(style = styles.backdrop)}
-    >
-      <Forecast {...forecastInfo} />
+    <ImageBackground source={require("../bg.png")} style={styles.backdrop}>
+      <View style={styles.background}>
+        <View style={styles.alignment}>
+          <Text style={styles.title}>Zip code is </Text>
+          <Text style={styles.zipCode}>{props.zipCode}</Text>
+        </View>
+        <Forecast {...forecastInfo} />
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   backdrop: {
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     width: "100%",
     height: "100%",
   },
+  background: {
+    backgroundColor: "rgba(192,192,192,0.3)",
+    height: "60%",
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+    lineHeight: 50
+  },
+  zipCode: {
+    fontSize: 32,
+    color: 'white',
+    lineHeight: 50
+  },
+  alignment: {
+    flexDirection: 'row'
+  }
 });
