d3481350df01f31789d0e1f0d851c539f1623822
NATTHAPHONG.P <6110110038@psu.ac.th>
2023-08-07 15:51:45 +0700

    Part6-1

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, ImageBackground } from "react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
-import 'react-native-gesture-handler';
-
 
 const availableZipItems = [
+  { place: "ฺBangkok", code: "10100" },
   { place: "Hatyai", code: "90110" },
   { place: "Trang", code: "92000" },
   { place: "Chiangmai", code: "50000" },
   { place: "Khonkaen", code: "40000" },
   { place: "Chonburi", code: "20000" },
+  { place: "Phuket", code: "83000" },
 ];
 
-const ZipItem = ({ place, code, navigation }) => (
+const ZipItem = ({ place, navigation }) => (
   <TouchableHighlight
     onPress={() => {
-      navigation.navigate("Weather", { zipCode: code });
+      navigation.navigate("Weather", { zipCode: place.code });
     }}
   >
     <View style={styles.ZipItem}>
-      <Text>{place}</Text>
-      <Text>{code}</Text>
+      <Text style={styles.ZipPlace}>{place.place}</Text>
     </View>
   </TouchableHighlight>
 );
@@ -29,26 +28,47 @@ const ZipItem = ({ place, code, navigation }) => (
 export default function ZipCodeScreen() {
   const navigation = useNavigation();
   return (
-    <View>
+    <ImageBackground source={require('../main-bg.jpg')} style={styles.backgroundImage}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>เลือกเมืองที่ต้องการ</Text>
+      </View>
       <FlatList
         data={availableZipItems}
         keyExtractor={(item) => item.code}
-        renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
+        renderItem={({ item }) => <ZipItem place={item} navigation={navigation} />}
       />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  ZipItem: {
+  backgroundImage: {
     flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  ZipItem: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+    marginVertical: 5,
+    borderRadius: 10,
+    width: '100%',
   },
   ZipPlace: {
-    flex: 1,
-  },
-  ZipCode: {
-    flex: 1,
+    fontSize: 18,
   },
 });
